6210fdd91d58248e3be83c22e721ae2b05aaf1e9
conorhastings <hastings.conorm@gmail.com>
2014-12-14 11:35:42 -0500

    working version of the game

--- app.jsx
+++ app.jsx
@@ -6,51 +6,123 @@
  	var TicTacToeGame = React.createClass({
 
  		getInitialState:function(){
- 			rows = [];
+
+ 	
+ 		return {board:this.makeBoard(),turn:"O"}
+ 		},
+ 		makeTurn: function(square, row, turn) {
+ 			var board = this.state.board;
+ 			if(board[row][square] == " "){
+ 				board[row][square] = turn;
+ 				if (turn== "O"){
+ 					var newTurn = "X";
+ 				}
+ 				else{
+ 					var newTurn = "O";
+ 				}
+ 				this.setState({board:board, turn:newTurn});
+
+ 			}
+ 			else{
+ 				alert("You can't go there!")
+ 			}
+ 			checkWin = this.winningBoard()
+ 			
+ 			if (checkWin[1] == true){
+ 			
+ 				alert(checkWin[0]+" click ok to start a new game")
+ 				this.newGame()
+ 			}
+ 			else if(checkWin[0] == "tie" ){
+ 				alert("the game has ended in a tie, click ok to start a new game")
+ 				this.newGame()
+ 			}
+
+ 		},
+
+ 		winningBoard:function(){
+ 			length = this.state.board.length
+ 			totalSquares = length * length
+ 			var flattenedBoard = this.state.board.reduce(function(a, b) {
+ 				return a.concat(b);
+ 			});
+ 			flattenedBoard = flattenedBoard.join('').split(' ').join('');
+ 		
+ 			var columnsArray = [];
+ 			leftDiagonal = [];
+ 			rightDiagonal = [];
+ 			var checkX = '';
+ 			var checkO = '';
+ 			for(var i = 0; i < length; i++){
+ 				var newColumn=[];
+ 				for(var n = 0; n < length; n++){
+
+ 					newColumn.push(this.state.board[length-(n+1)][i]);
+ 				}
+ 				leftDiagonal.push(this.state.board[i][i]);
+ 				rightDiagonal.push(this.state.board[i][length-(i+1)]);
+ 				columnsArray.push(newColumn);
+ 				checkX = checkX + 'X';
+ 				checkO = checkO + 'O';
+ 			}
+ 			for(var k = 0; k < length; k++){
+ 				if((this.state.board[k].join('') == checkX) || (this.state.board[k].join('') == checkO)){
+ 					
+ 					return [this.state.board[k][0]+" has won in row "+ (k+1),true];
+ 				}else if((columnsArray[k].join('') == checkX)||(columnsArray[k].join('') == checkO)){
+ 					
+ 					return [columnsArray[k][0]+" has won in column " + (k+1),true];
+
+ 				}	else if((leftDiagonal.join('') == checkX) || (leftDiagonal.join('') == checkO)){
+ 					
+ 					return [leftDiagonal[0] + " has won on left diagonal",true];
+
+ 				} else if((rightDiagonal.join('') == checkX) || (rightDiagonal.join('') == checkO)){
+ 					
+ 					return [rightDiagonal[0] + " has won on right diagonal",true];
+
+ 				}else if(flattenedBoard.length == totalSquares){
+
+ 					return ["tie" , false]
+
+ 				}
+ 			}
+
+ 			return ["no winner",false];
+ 		},
+
+ 		newGame:function(){
+ 			this.setState({board:this.makeBoard(), turn:"O"});
+ 		},
+ 		makeBoard:function(){
+ 			var board = [];
  			for(var i=0; i < this.props.rows; i++){
  				row = []
  				for(var j=0; j < this.props.rows; j++){
  					row.push(" ")
 
  				}
- 				rows.push(row)
-
+ 				board.push(row)
  			}
-
-
- 			return {rows:rows,turn:"O"}
- 		},
- 		clickBox: function(square, row, turn) {
- 			var rows = this.state.rows;
- 			rows[row][square] = turn
- 			if (turn== "O"){
- 				var newTurn = "X"
- 			}
- 			else{
- 				var newTurn = "O"
- 			}
- 			console.log(rows)
-
- 			this.setState({rows:rows, turn:newTurn})
+ 			return board;
 
  		},
 
- 		winningBoard:function(){
-
- 		},
 
  		render: function(){
- 			myTurn = this.state.turn;
- 			clickBox = this.clickBox;
+ 			var myTurn = this.state.turn;
+ 			var makeTurn = this.makeTurn;
  			return <div>
+ 			<h1>Welcome to React Tac Toe</h1>
  			<h1>Current Turn is: {this.state.turn}</h1>
+ 			<button className="btn btn-danger" onClick = {this.newGame}>Restart Game</button><br /><br />
  			<table>
- 			{this.state.rows.map(function(row,index){
+ 			{this.state.board.map(function(row,index){
  				return(<tr key={index}>
  					{row.map(function(square,location){
-
- 						return( <Square status={square} key = {location} location = {location} row = {index} turn={myTurn} clickBox={clickBox} />);
- 						console.log()
+ 					
+ 						return( <Square play = {square} key = {location} location = {location} row = {index} turn = {myTurn} makeTurn = {makeTurn} />);
+ 						
  					})}
  					</tr>);
  			})}
@@ -62,31 +134,20 @@
  		}
  	});
 
+var Square = React.createClass({
 
+	handleClick: function(){
+	
+		this.props.makeTurn(this.props.location, this.props.row,this.props.turn);
+	
+	},
+	render:function(){
+		return <td className="square" onClick={this.handleClick}><h3>{this.props.play}</h3></td>
+	}
 
+})
 
- 	var Square = React.createClass({
-
- 		getInitialState:function(){
- 			return {square: " "}
- 		},
-
- 		handleClick: function(){
- 			console.log('clicked')
- 			this.props.clickBox(this.props.location, this.props.row,this.props.turn);
- 			this.setState({square:this.props.turn})
-
- 		},
- 		render:function(){
- 			return <td className="square" onClick={this.handleClick}><h3>{this.state.square}</h3></td>
- 		}
-
- 	})
-
-
-	// React.render(<Square /> , document.getElementById('board'));
-
-	React.render(<TicTacToeGame rows="5" />, document.getElementById('targetEl'));
+React.render(<TicTacToeGame rows="3" />, document.getElementById('targetEl'));
 
 
 })();
