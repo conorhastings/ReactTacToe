f6db7bca5d2bdff5afb8bba224935aa9bb867ba7
conorhastings <hastings.conorm@gmail.com>
2014-12-13 22:16:41 -0500

    better understanding of props, getting somewhere close to an actual tic tac toe game

--- app.jsx
+++ app.jsx
@@ -1,48 +1,65 @@
-(function(){
+/**
+ * @jsx React.DOM
+ */
+ (function(){
 
-	var TicTacToeGame = React.createClass({
+ 	var TicTacToeGame = React.createClass({
 
-		getInitialState:function(){
-			rows = [];
-			for(var i=0; i < this.props.rows; i++){
-				row = []
-				for(var j=0; j < this.props.rows; j++){
-					row.push(" ")
+ 		getInitialState:function(){
+ 			rows = [];
+ 			for(var i=0; i < this.props.rows; i++){
+ 				row = []
+ 				for(var j=0; j < this.props.rows; j++){
+ 					row.push(" ")
 
-				}
-				rows.push(row)
+ 				}
+ 				rows.push(row)
 
-			}
-			console.log(rows)
-			return {rows:rows,turn:"o"}
-		},
+ 			}
 
-		render: function(){
-			return (
-				
-				<table>
-			{this.state.rows.map(function(row){
-			return(<tr>
-				{row.map(function(square){
-					return(<Square />)
-					console.log()
-				})}
-			</tr>)
-			})}
-			</table>
-			)
+ 			return {rows:rows,turn:"O"}
+ 		},
+ 		clickBox: function(square, row, player) {
+ 			console.log('hello')
+			},
+
+ 		render: function(){
+ 			myTurn = this.state.turn;
+ 			clickBox = this.clickBox;
+ 			return <div>
+ 			<h1>Current Turn is: {this.state.turn}</h1>
+ 			<table>
+ 				{this.state.rows.map(function(row,index){
+ 					return(<tr key={index}>
+ 						{row.map(function(square,location){
+ 	
+ 							return( <Square status={square} key = {location} location = {location} row = {index} turn={myTurn} clickBox={clickBox} />);
+ 							console.log()
+ 						})}
+ 						</tr>);
+ 				})}
+ 				</table>
+ 				</div>
 
 
-		}
-	});
+
+ 		}
+ 	});
 
 
-var Square = React.createClass({
-	render:function(){
-		return <td className="square"></td>
-	}
 
-})
+
+ 	var Square = React.createClass({
+
+ 		handleClick: function(){
+ 			console.log('clicked')
+ 			this.props.clickBox(this.props.location, this.props.row,this.props.turn);
+ 		},
+ 		render:function(){
+ 			return <td className="square" onClick={this.handleClick}></td>
+ 		}
+
+ 	})
 
 
 	// React.render(<Square /> , document.getElementById('board'));
